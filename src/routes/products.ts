babca427c0be02a3fcd75eb22a7b1c824568df34
abdalbenaesha <abdal@invisible-systems.com>
2024-01-28 15:38:36 +0000

    Modify purchaseProduct POST api endpoint to adjust stock quantity for a matching product - #3

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,33 +1,53 @@
-import express from "express";
-import { Product } from "../entities/Product";
-import { AppDataSource } from "../data-source";
+import express from 'express';
+import { Product } from '../entities/Product';
+import { AppDataSource } from '../data-source';
 
 const router = express.Router();
 
 // Route to get all products
-router.get("/products", async (req, res) => {
-    try {
-        const products = await AppDataSource.manager.find(Product);
-        res.json(products);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+router.get('/products', async (req, res) => {
+  try {
+    const products = await AppDataSource.manager.find(Product);
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 //purchase product
-router.post("/purchaseProduct/:productId", async (req, res) => {
-    try {
-        const product = await AppDataSource.manager.findOne(Product, { where: { id: parseInt(req.params.productId) } });
-        if (product) {
-            await AppDataSource.manager.save(product);
-            res.json(product);
-        } else {
-            res.status(404).json({ error: "Product not found" });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
+router.post('/purchaseProduct/:id', async (req, res) => {
+  const productId = parseInt(req.params.id);
+  const purchaseQuantity = parseInt(req.body.quantity);
+
+  if (isNaN(productId) || isNaN(purchaseQuantity) || purchaseQuantity <= 0) {
+    return res.status(400).json({ error: 'Invalid product ID' });
+  }
+
+  try {
+    const product = await AppDataSource.manager.findOne(Product, {
+      where: { id: productId },
+    });
+
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
     }
+
+    if (product.stock < purchaseQuantity) {
+      return res.status(400).json({ error: 'Insufficient stock available' });
+    }
+
+    if (product.stock <= 0) {
+      return res.status(400).json({ error: 'Product is out of stock' });
+    }
+
+    product.stock -= purchaseQuantity;
+    await AppDataSource.manager.save(product);
+
+    res.status(201).json({ message: 'Product updated successfully', product });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 export default router;
